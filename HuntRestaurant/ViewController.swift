732e94d9daf0823ae9202bf2c898c4a5b92b2c83
Mengistayehu Mamo <mamushitilayemamo@gmail.com>
2016-04-09 08:30:30 -0700

    Initial Commit

---
+++ HuntRestaurant/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HuntRestaurant
+//
+//  Created by Mengistayehu Mamo on 4/9/16.
+//  Copyright © 2016 Mengistayehu Mamo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
